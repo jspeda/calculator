720c047394aef7cf7b0e8f6f7eef776aad0232e0
Joseph Spedale <jspedale@gmail.com>
2016-12-22 09:44:41 -0500

    Chaining operations works as intended, does not calculate until equals is pressed

--- app.js
+++ app.js
@@ -1,54 +1,32 @@
 $(document).ready(function() {
-var number = "";
-var secondNumber = "";
-var operator = "";
-var newNumber = "";
-var readout = $(".readout");
-var answer = "";
-$(".num").click(function() {
-  number += $(this).html();
-  console.log(number);
-  readout.html(number);
-});
-$(".operator").not(".equals").click(function() {
-  console.log($(this));
-  operator = $(this).html();
-  newNumber = number;
-  number = "";
-  readout.html("0");
-});
-$(".c, .ca").click(function() {
-  number = "";
-  readout.html("0");
-  if ($(this).attr("class") === "ca") {
-    newNumber = "";
-  }
-  else {
-    newNumber = "";
-    number = "";
-    readout.html("");
-  }
-});
-$(".equals").click(function() {
-  floatNumber = parseFloat(number);
-  floatNewNumber = parseFloat(newNumber);
-  if (operator === "+") {
-    answer = floatNewNumber + floatNumber;
-    newNumber = answer;
-    readout.html(answer);
-  }
-  else if (operator === "*") {
-    answer = floatNewNumber * floatNumber;
-    newNumber = answer;
-    readout.html(answer);
-  }
-  else if (operator === "/") {
-    answer = floatNewNumber / floatNumber;
-    readout.html(answer);
-  }
-  else if (operator === "-") {
-    answer = floatNewNumber - floatNumber;
-    readout.html(answer);
-  }
-});
+  var operator = "";
+  var readout = $(".readout");
+  var accumulation = [];
+  $(".num").click(function() {
+    accumulation.push($(this).html());
+    console.log(number);
+    readout.html(accumulation.join(''));
+  });
+  $(".operator").not(".equals").click(function() {
+    console.log($(this));
+    operator = $(this).html();
+    accumulation.push($(this).html());
+    readout.html(accumulation.join(''));
+  });
+  $(".c, .ca").click(function() {
+    if ($(this).attr("class") === "ca") {
+      accumulation = [];
+      readout.html = "";
+    }
+    else {
+      accumulation.pop();
+      readout.html(accumulation.join(''));
+    }
+  });
+  $(".equals").click(function() {
+    floatNumber = parseFloat(number);
+    floatNewNumber = parseFloat(newNumber);
+    console.log(eval(accumulation.join('')));
+    readout.html(eval(accumulation.join('')));
+  });
 })
